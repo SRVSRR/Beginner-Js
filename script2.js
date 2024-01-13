fc6cdc54e277fd29e1b7d8f226b7f08c808a795e
Rohan Nandan <srvsrr@Rohans-MacBook-Air.local>
2024-01-13 21:22:49 +1200

    if else

--- script2.js
+++ script2.js
@@ -1,7 +1,14 @@
 const prompt = require("prompt-sync")();
 
 const target =  Math.round(Math.random() *100);
-console.log(target);
 
-let guess = 0;
+let guesses = 0;
+
+console.log(target)
 const guess = Number(prompt("Guess the number (0-100):"))
+
+if (guess>target) {
+    console.log("Your guess is too high");
+} else if (guess<target){
+    console.log("Your guess is too low");
+}
