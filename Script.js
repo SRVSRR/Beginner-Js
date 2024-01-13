fcda3d0146bde1ae8906e9e6f08b7080fd0ffb02
Rohan Nandan <srvsrr@Rohans-MacBook-Air.local>
2024-01-13 20:50:34 +1200

    percent

--- Script.js
+++ Script.js
@@ -2,26 +2,37 @@ const prompt = require("prompt-sync")();
 
 console.log("Welcome to the computer hardware");
 
-const answer1 = prompt("What is the brain of the computer?");
-const correct_answer1 = "CPU";
+let correctAnswers = 0;
+const totalQuestions = 3;
 
-if (answer1.toUpperCase() === correct_answer1) {
+const answer1 = prompt("What is the brain of the computer?");
+const correctAnswer1 = "CPU";
+
+if (answer1.toUpperCase() === correctAnswer1) {
     console.log("You got it correct");
+    correctAnswers++;
 } else {    console.log("You got it wrong");
 }
 
 const answer2 = prompt("What is the brain of the computer?");
-const correct_answer2 = "CPU";
+const correctAnswer2 = "CPU";
 
-if (answer2.toUpperCase() === correct_answer2) {
+if (answer2.toUpperCase() === correctAnswer2) {
     console.log("You got it correct");
+    correctAnswers++;
 } else {    console.log("You got it wrong");
 }
 
 const answer3 = prompt("What is the recommended amount of RAM?");
-const correct_answer3 = "16GB";
+const correctAnswer3 = "16GB";
 
-if (answer3.toUpperCase() === correct_answer3) {
+if (answer3.toUpperCase() === correctAnswer3) {
     console.log("You got it correct");
+    correctAnswers++;
 } else {    console.log("You got it wrong");
 }
+
+const percent = Math.round((correctAnswers / totalQuestions) * 100);
+
+console.log("You got", correctAnswers, "questions correct");
+console.log("You got", percent, "percent");
