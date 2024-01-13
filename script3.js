9922ae2b37825c2be585245d7eb6815f6f9a2d37
Rohan Nandan <srvsrr@Rohans-MacBook-Air.local>
2024-01-13 21:50:16 +1200

    r,p,s

---
+++ script3.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
+const choice = prompt("Enter rock,paper or scissors: ")
+if (choice === "rock" || choice == "paper" || choice == "scissors") {
+
+}
