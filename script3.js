83ad7bc87187f68af84288562615514cec602fb5
Rohan Nandan <srvsrr@Rohans-MacBook-Air.local>
2024-01-15 00:19:33 +1200

    won or draw

--- script3.js
+++ script3.js
@@ -5,6 +5,11 @@ let losses = 0;
 let ties = 0;
 
 const choice = prompt("Enter rock,paper or scissors: ")
-if (choice === "rock" || choice == "paper" || choice == "scissors") {
-
+if (choice !== "rock" && choice !== "paper" & choice !== "scissors") {
+    console.log("Please enter a valid choice.");
 }
+
+const choices = ["rock", "paper", "scissor"] ;
+const randomIndex =  Math.round(Math.random() * 2);
+const computerChoice = choices[randomIndex];
+console.log(computerChoice);
